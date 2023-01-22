2d29ca67b8aa89eab569514baeb133105dde6116
Mayuresh A Nirhali <mayureshnirhali@gmail.com>
2023-01-22 13:04:19 +0530

    add cancel_reason_id

--- src/main/java/in/succinct/beckn/ondc/retail/Message.java
+++ src/main/java/in/succinct/beckn/ondc/retail/Message.java
@@ -9,4 +9,11 @@ public class Message extends in.succinct.beckn.Message {
     public void setOrderId(String order_id){
         set("order_id",order_id);
     }
+
+    public String getCancellationReasonId(){
+        return get("cancellation_reason_id");
+    }
+    public void setCancellationReasonId(String cancellation_reason_id){
+        set("cancellation_reason_id",cancellation_reason_id);
+    }
 }
